c4c0220c34fe87cf0c6ad418a8ef892b86e1e8e5
JuliaOliver <74153919+JuliaOliver@users.noreply.github.com>
2020-11-13 17:22:22 +0200

    Create password_reset.py

---
+++ Fundamentals/Exams/password_reset.py
@@ -0,0 +1,29 @@
+password_to_change = input()
+
+command = input()
+# password_to_change = password_to_change
+
+while command != "Done":
+    if command == "TakeOdd":
+        password_to_change = password_to_change[1:len(password_to_change):2]
+        print(password_to_change)
+
+    elif command.split()[0] == "Cut":
+        index = int(command.split()[1])
+        length = int(command.split()[2])
+        remove = password_to_change[index:index + length]
+        password_to_change = password_to_change.replace(remove, "", 1)
+        print(password_to_change)
+
+    elif command.split()[0] == "Substitute":
+        move_out = command.split()[1]
+        move_in = command.split()[2]
+        if move_out not in password_to_change:
+            print("Nothing to replace!")
+        else:
+            password_to_change = password_to_change.replace(move_out, move_in)
+            print(password_to_change)
+
+    command = input()
+
+print(f"Your password is: {password_to_change}")
