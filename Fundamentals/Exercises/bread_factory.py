49b4b9fdefed4bbaeb79499285f3f5c47d619209
JuliaOliver <74153919+JuliaOliver@users.noreply.github.com>
2020-11-13 17:29:08 +0200

    Create bread_factory.py

---
+++ Fundamentals/Exercises/bread_factory.py
@@ -0,0 +1,46 @@
+events = input().split("|")
+
+energy = 100
+coins = 100
+is_bankrupt = False
+
+for event in events:
+    arguments = event.split("-")
+    name = arguments[0]
+    value = int(arguments[1])
+
+    if name == "rest":
+        gained_energy = 0
+
+        if energy + value < 100:
+            gained_energy = value
+            energy += value
+        else:
+            gained_energy = 100 - energy
+            energy = 100
+
+        print(f"You gained {gained_energy} energy.")
+        print(f"Current energy: {energy}.")
+
+    elif name == "order":
+        if energy < 30:
+            energy += 50
+            print("You had to rest!")
+            continue
+
+        coins += value
+        energy -= 30
+        print(f"You earned {value} coins.")
+    else:
+        if coins <= value:
+            print(f"Closed! Cannot afford {name}.")
+            is_bankrupt = True
+            break
+
+        coins -= value
+        print(f"You bought {name}.")
+
+if not is_bankrupt:
+    print(f"Day completed!")
+    print(f"Coins: {coins}")
+    print(f"Energy: {energy}")
